261cb7b332ce26d79df6014a2c000269d479106b
RafaelGSS <rafael.nunu@hotmail.com>
2024-12-03 14:15:29 -0300

    feat: check if ncu-ci exists

--- end-ci.sh
+++ end-ci.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 CI="$1"
 
+# Check if ncu-ci program exists
+if ! command -v ncu-ci &> /dev/null; then
+  echo "Error: ncu-ci is not installed or not found in PATH."
+  exit 1
+fi
+
 ncu-ci url $CI 2>&1 | awk '/Result .*/ { exit $2 == "PENDING" }';
